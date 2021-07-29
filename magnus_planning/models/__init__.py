e31ece7a82487d824fa44035d4b91a11541bb67d
DeekshithSystemsValley <deekshith@systemsvalley.co.in>
2021-07-29 11:08:51 +0530

    Added magnus planning

---
+++ magnus_planning/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import account_analytic_line
+from . import magnus_planning
+# from . import magnus_plan
+
