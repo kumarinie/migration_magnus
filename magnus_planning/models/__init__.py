81ec5c1ca288e7137f91ddc2c7af62e5ff46940b
kumarinie <kuamrisushmanie@gmail.com>
2022-01-06 00:54:07 +0530

    delete test magnus planning module Module: magnus_planning

--- magnus_planning/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import account_analytic_line
-from . import magnus_planning
-from . import timesheet_analytic_line
-
